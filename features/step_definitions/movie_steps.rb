805b69e28105a1a7e52fa3057c66dc66506da327
tobe101 <circlesdusoleil@googlemail.com>
2012-10-24 22:51:28 +0300

    Update features/step_definitions/movie_steps.rb
    Comment pointed out nonexistent "page.content"; changed to "page.body" (as in HW instructions)

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -13,7 +13,7 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
+  #  page.body is the entire content of the page as a string.
   flunk "Unimplemented"
 end
 
